53b87f75fe5f9dc1ad9a86f03555246ea6420ad2
Ana-Hermina Ghenu <EvoNerd@users.noreply.github.com>
2025-05-14 20:54:30 +0200

    optimized code, grid layout

---
+++ app--old.R
@@ -0,0 +1,217 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(tidyverse)
+#library(patchwork) # for combining multiple plots into 1
+
+###################################
+# create necessary functions
+###################################
+
+
+
+
+###################################
+# make the R Shiny app
+###################################
+
+#user interface
+ui <- fluidPage(pageWithSidebar( 
+  
+  headerPanel = headerPanel("Diploid model of selection with underdominance"),
+  
+  sidebarPanel(
+    
+    HTML("<p style='font-size:14px'><B>Fitness of AA (wAA)= 1.2<BR>
+         Fitness of aa (waa)= 1.0<BR>
+         Fitness of Aa (wAa)= 1 - h_under</B>"),
+
+    sliderInput(inputId = "h_under", label = "Underdominance coefficient (h_under)", value = -0.2, 
+                min = -1, max = 1, step = 0.1),
+    
+    sliderInput(inputId = "p_0", label = "Initial p", value = 0.32, 
+                min = 0, max = 1, step = 0.02),
+    
+    sliderInput(inputId = "gen", label = "Number of generations", value = 50, 
+                min = 5, max = 1000, step = 5),
+  
+    HTML("<p style='font-size:8px'>
+          Modified from: Copyright (c) 2017 Silas Tittes, MIT License, <A href='https://github.com/silastittes/shiny_popgen'>https://github.com/silastittes/shiny_popgen</a></p>")
+    
+  ), 
+  
+  mainPanel =  mainPanel(
+    plotOutput(outputId = 'viz'),
+    
+    selectInput("select", label = "Plot options", 
+                choices = list("Mean fitness by p" = 1,
+                               "Change in p by p" = 2,
+                               "Allele frequency over time" = 3,
+                               "Genotypes over time" = 4,
+                               "Mean fitness over time" = 5), selected = 4)
+    
+    
+  )
+))
+
+#back end code and response to user input
+server <- function(input, output){
+  
+  output$viz <- renderPlot({
+    
+    p <- seq(0, 1, length.out = 1000)
+    #parameters
+    wAA = 1.2
+    h_under = input$h_under
+    waa = 1.0
+    p_0 = input$p_0
+    gen = (input$gen)+1
+    t = seq(0, gen, length.out = 1000)
+    
+    # calculate the fitness of the heterozygote using the dominance coefficient,
+    wAa <- 1 + h_under
+      
+    # make a plot of the fitness landscape
+    schematic_plot <- data.frame(Genotype = c("aa", "aA", "Aa", "AA"),
+                            Fitness = c(waa, rep(wAa, 2), wAA)) %>% 
+                    ggplot(aes(x=Genotype, y=Fitness, fill=Genotype)) +
+                      geom_bar(stat="identity") +
+                      scale_fill_manual(values = c("AA" = "firebrick", "Aa" = "orange", "aA" = "orange", "aa" = "gold")) +
+                      theme_classic()+theme(text = element_text(size=20),
+                                            legend.position="none")
+    
+
+    
+    #if(input$w_plot){
+    if(input$select == 1){
+      W <- p^2*wAA + 2*p*(1-p)*wAa + (1-p)^2*waa
+      selected_plot <- data.frame(p=p, W=W) %>%
+        ggplot(aes(x = p, y = W)) +
+        geom_line(color="forestgreen", size=2) +
+        xlab("Mean Fitness") +
+        xlab("Allele frequency, p") +
+#        xlim(0, 1) +
+        scale_x_continuous(expand = c(0, 0), limits = c(0, 1)) + 
+        scale_y_continuous(expand = c(0, 0), limits = c(min(waa,wAa,wAA)-0.001, max(waa,wAa,wAA)+0.001)) + 
+#        theme(panel.background =  
+#                element_rect(fill =  rgb(30, 144, 255, 25, 
+#                                         maxColorValue = 255)),
+#              text = element_text(size=16, family= "Times"))
+        theme_classic()+theme(text = element_text(size=20))
+      
+      #} else if(input$delta_plot){
+    } else if(input$select == 2){
+      W <- p^2*wAA + 2*p*(1-p)*wAa + (1-p)^2*waa
+      delta_p <- (p^2*wAA + p*(1-p)*wAa) / W - p
+      setlim <- (max(waa,wAa,wAA)-min(waa,wAa,wAA))/(wAA + wAa + waa)
+      setlimround <- round(10*setlim)/10+0.1
+      selected_plot <- data.frame(p=p, delta_p=delta_p) %>%
+        ggplot(aes(x = p, y = delta_p)) +
+        geom_line(color="firebrick", size=2) +
+        geom_hline(yintercept = 0, lty = 2) +
+        ylab(expression(paste(Delta,p))) +
+        xlab("Allele frequency, p") +
+#        xlim(0, 1) +
+        scale_x_continuous(expand = c(0, 0), limits = c(0, 1)) + 
+        scale_y_continuous(expand = c(0, 0), limits = c(-setlimround,setlimround)) + 
+#        theme(panel.background =  
+#                element_rect(fill =  rgb(30, 144, 255, 25, 
+#                                         maxColorValue = 255)),
+#              text = element_text(size=16, family= "Times"))
+        theme_classic()+theme(text = element_text(size=20))
+      
+      #} else if(input$time_plot){
+    } else if(input$select == 3){
+      
+      p_t <- rep(NA, gen)
+      p_t[1] <- p_0
+      for(i in 2:gen){
+        W <- p_t[i-1]^2*wAA + 2*p_t[i-1]*(1-p_t[i-1])*wAa + (1-p_t[i-1])^2*waa
+        p_t[i] <- (p_t[i-1]^2 * wAA + p_t[i-1] * (1-p_t[i-1]) * wAa) / W
+      }
+      
+      selected_plot <- data.frame(t = 1:gen, p_t = p_t) %>%
+        ggplot(aes(x = t-1, y = p_t)) +
+        geom_point(color="firebrick", size=2) +
+        xlab("Generation") +
+        ylab("Allele frequency, p") +
+        annotate("text", x = 0.9*gen, y = 0.14, label = "Final p")+
+        annotate("text", x = 0.9*gen, y = 0.1, label = round(p_t[gen],5))+
+#        ylim(0, 1) + 
+        scale_x_continuous(expand = c(0, 0), limits = c(0, input$gen+2)) + 
+        scale_y_continuous(expand = c(0, 0), limits = c(0, 1.01)) + 
+#        theme(panel.background =  
+#                element_rect(fill =  rgb(30, 144, 255, 25, 
+#                                         maxColorValue = 255)),
+#              text = element_text(size=16, family= "Times"))
+        theme_classic()+theme(text = element_text(size=20))
+    } else if(input$select == 4){
+      
+      p_t <- rep(NA, gen)
+      p_t[1] <- p_0
+      for(i in 2:gen){
+        W <- p_t[i-1]^2*wAA + 2*p_t[i-1]*(1-p_t[i-1])*wAa + (1-p_t[i-1])^2*waa
+        p_t[i] <- (p_t[i-1]^2 * wAA + p_t[i-1] * (1-p_t[i-1]) * wAa) / W
+      }
+      
+      # simulate forward in time for each genotype
+      temp_df <- data.frame(t = 1:gen, p_t = p_t)
+      temp_df <- rbind(cbind(temp_df, y=(1-temp_df$p_t)^2, Genotype = "aa"),
+                       cbind(temp_df, y=2*temp_df$p_t*(1-temp_df$p_t), Genotype = "aA & Aa"),
+                       cbind(temp_df, y=temp_df$p_t^2, Genotype = "AA"))
+      temp_df$Genotype <- factor(temp_df$Genotype,
+                                 levels = c("aa", "aA & Aa", "AA"))
+        
+      selected_plot <-  temp_df %>%
+        ggplot(aes(x = t-1, y = y, colour=Genotype)) +
+        geom_point() +
+        scale_color_manual(values = c("AA" = "firebrick", "aA & Aa" = "orange", "aa" = "gold")) +
+        labs(x= "Generation",y="Genotype frequency",
+             color = "Legend") +
+        scale_x_continuous(expand = c(0, 0), limits = c(0, input$gen+2)) + 
+        scale_y_continuous(expand = c(0, 0), limits = c(0, 1.01)) + 
+        #        theme(panel.background =  
+        #                element_rect(fill =  rgb(30, 144, 255, 25, 
+        #                                         maxColorValue = 255)),
+        #              text = element_text(size=16, family= "Times")) 
+        labs(colour = "Genotypes") +
+        theme_classic()+theme(text = element_text(size=20),
+                              legend.position="bottom")
+    } else if(input$select == 5){
+      p_t <- rep(NA, gen)
+      W_t <- rep(NA, gen)
+      p_t[1] <- p_0
+      W_t[1] <- p_0^2*wAA + 2*p_0*(1-p_0)*wAa + (1-p_0)^2*waa
+      for(i in 2:gen){
+        W_t[i] <- p_t[i-1]^2*wAA + 2*p_t[i-1]*(1-p_t[i-1])*wAa + (1-p_t[i-1])^2*waa
+        p_t[i] <- (p_t[i-1]^2 * wAA + p_t[i-1] * (1-p_t[i-1]) * wAa) / W_t[i]
+      }
+      
+      selected_plot <- data.frame(t = 1:gen, W_t = W_t) %>%
+        ggplot(aes(x = t-1, y = W_t)) +
+        geom_point(color="forestgreen", size=2) +
+        xlab("Generation") +
+        ylab("Mean fitness") +
+        #        ylim(0, 1) + 
+        scale_x_continuous(expand = c(0, 0), limits = c(0, input$gen+2)) + 
+        scale_y_continuous(expand = c(0, 0), limits = c(min(waa,wAa,wAA)-0.001, max(waa,wAa,wAA)+0.001)) + 
+        #        theme(panel.background =  
+        #                element_rect(fill =  rgb(30, 144, 255, 25, 
+        #                                         maxColorValue = 255)),
+        #              text = element_text(size=16, family= "Times"))
+        theme_classic()+theme(text = element_text(size=20))
+    }
+    
+    return(wrap_plots(schematic_plot, 
+                      selected_plot, 
+                      nrow = 2, ncol = 1, byrow = FALSE))
+  })
+}
+# Run the application 
+shinyApp(ui = ui, server = server)
--- app.R
+++ app.R
@@ -1,217 +1,229 @@
 #
+# author: Ana-Hermina Ghenu
+# contact: ana.hermina.ghenu at gmail dot com
+# date created: 14 May 2025
+#
+#--------------------------------------------------
 # This is a Shiny web application. You can run the application by clicking
 # the 'Run App' button above.
 #
 # Find out more about building applications with Shiny here:
-#
 #    http://shiny.rstudio.com/
 #
 
-library(tidyverse)
-library(patchwork) # for combining multiple plots into 1
+# load the environment
+
+#library(tidyverse) # for ggplot and pipe syntax
+library(shiny) # for app
+#library(patchwork) # for combining multiple plots into 1
+library(bslib) # for most recent recommended UI options
+
+# set the default plot style
+fave_theme <- theme_light() + # see other options at https://ggplot2.tidyverse.org/reference/ggtheme.html
+  theme(text = element_text(size=19), # larger text size for titles & axes
+        panel.grid.major = element_blank(), # remove major gridlines
+        panel.grid.minor = element_blank()) # remove minor gridlines
+theme_set(fave_theme)
+
+# increase the default thickness of lines for all geom objects in ggplot2
+update_geom_defaults("line", list(linewidth = 2.5, alpha=0.8))
+
+# define colour scheme for genotypes
+colours_genotypes <- c("AA" = "firebrick", "Aa" = "orange", "aA" = "orange", "aa" = "gold")
 
 ###################################
-# create necessary functions
+# define simulation functions
 ###################################
 
+# recursion equation: a function to get the allele frequency in the next generation
+next_p <- function(w_AA, w_heterozygote, current_p, W_bar){
+  output <- (current_p^2*w_AA + current_p*(1-current_p)*w_heterozygote) / W_bar
+  return(unname(output))
+}
 
+# a function to get the fitness of the genotypes
+fitness_genotypes <- function(sel_coef, dom_coef)
+  return(c(aa = 1.0, aA = 1 + dom_coef, Aa = 1 + dom_coef, AA = 1 + sel_coef))
+
+# a function to get mean fitness of the population
+mean_fitness <- function(w_genotypes, freq){
+  output <- freq^2*w_genotypes["AA"] + 2*freq*(1-freq)*w_genotypes["Aa"] + (1-freq)^2*w_genotypes["aa"]
+  return(unname(output))
+}
+
+# a function to simulate genotypes over time
+sim_forward_time <- function(num_gens, freq_init, w_genotypes){
+  # initialize data.frame for storage starting from initial allele frequencies at generation 0
+  df <- data.frame(gens = 0:num_gens,
+                   p = c(freq_init, rep(NA, num_gens)))
+  # loop through each generation
+  for(i in 2:nrow(df)){
+    W_bar <- mean_fitness(w_genotypes, df$p[i-1])
+    # use current mean fitness to get allele frequency in the next generation
+    df$p[i] <- next_p(w_genotypes["AA"], w_genotypes["Aa"], df$p[i-1], W_bar)
+  }
+  # finally, use the allele frequencies for each generation to get the genotypes
+  df <- df %>% mutate(aa = (1-p)^2,
+                      hetero = 2*p*(1-p),
+                      AA = p^2)
+  return(df)
+}
+
+###################################
+# define visualization functions
+###################################
+
+# plot fitness of the genotypes
+plot_schematic <- function(w_genotypes) {
+  # create df for nice plotting
+  df <- data.frame(Fitness = w_genotypes,
+                   Genotype = names(w_genotypes))
+  # boxplot of fitness
+  ggplot(df,
+         aes(x = Genotype, y = Fitness, fill = Genotype)) +
+    geom_bar(stat="identity") +
+    scale_fill_manual(values = colours_genotypes) +
+    theme(legend.position="none") +
+    scale_y_continuous(expand = c(0, 0))
+}
+
+# a function to plot the genotypes over time
+plot_t_genotypes <- function(sim_df){
+  # remove the column for allele frequency and change the df to long format
+  plot_df <- sim_df %>% select(-p) %>%
+    pivot_longer(!gens, names_to="genotype", values_to="freq")
+  # make genotype a factor with levels for nice plotting
+  plot_df$genotype <- factor(plot_df$genotype,
+                             levels = c("AA", "hetero", "aa"))
+  # rename the factor levels for nice plotting
+  levels(plot_df$genotype) <- c("AA", "aA & Aa", "aa")
+  ggplot(plot_df,
+         aes(x=gens, y=freq, colour=genotype)) +
+    geom_line() +
+    scale_color_manual(values = unname(colours_genotypes)[-2]) +
+    labs(x= "Generation", y="Genotype frequency", color = "Legend") +
+    scale_y_continuous(limits = c(-0.01, 1.01), expand = c(0, 0)) +
+    # can try removing the legend altogether
+    theme(legend.position="none")
+}
+
+# a function to plot the allele frequency over time
+plot_t_allele <- function(sim_df){
+  # keep just the columns for time and allele frequency
+  plot_df <- sim_df %>% select(gens, p)
+  ggplot(plot_df,
+         aes(x=gens, y=p)) +
+    geom_line() +
+    labs(x= "Generation", y="Allele frequency") +
+    scale_y_continuous(limits = c(-0.01, 1.01), expand = c(0, 0))
+}
+
+# a function to plot delta p
+plot_p_byp <- function(w_genotypes){
+  # initialize a vector of allele frequencies from 0 to 1
+  plot_df <- data.frame(p = seq(0, 1, length.out = 1000)) %>%
+    # get the mean fitness at each allele frequency
+    mutate(W_bar = mean_fitness(w_genotypes, p),
+           # get delta_p by current allele frequency from that in the next generation
+           delta_p = next_p(w_genotypes["AA"], w_genotypes["Aa"], p, W_bar) - p)
+  # figure out how to draw the arrows
+  # TO DO!!
+  ggplot(plot_df,
+         aes(x=delta_p, y=p)) +
+    geom_vline(xintercept = 0, linewidth=0.6, linetype = "dotted") +
+    geom_point() +
+    labs(x= expression("Change in allele frequency (" * Delta * "p)"), y="Allele frequency") +
+    scale_y_continuous(limits = c(-0.01, 1.01), expand = c(0, 0))
+}
 
 
 ###################################
 # make the R Shiny app
 ###################################
 
-#user interface
-ui <- fluidPage(pageWithSidebar( 
-  
-  headerPanel = headerPanel("Diploid model of selection with underdominance"),
-  
-  sidebarPanel(
-    
-    HTML("<p style='font-size:14px'><B>Fitness of AA (wAA)= 1.2<BR>
-         Fitness of aa (waa)= 1.0<BR>
-         Fitness of Aa (wAa)= 1 - h_under</B>"),
 
-    sliderInput(inputId = "h_under", label = "Underdominance coefficient (h_under)", value = -0.2, 
-                min = -1, max = 1, step = 0.1),
-    
-    sliderInput(inputId = "p_0", label = "Initial p", value = 0.32, 
-                min = 0, max = 1, step = 0.02),
-    
-    sliderInput(inputId = "gen", label = "Number of generations", value = 50, 
-                min = 5, max = 1000, step = 5),
+# user interface with fluidPage
+ui <- fluidPage(
   
-    HTML("<p style='font-size:8px'>
-          Modified from: Copyright (c) 2017 Silas Tittes, MIT License, <A href='https://github.com/silastittes/shiny_popgen'>https://github.com/silastittes/shiny_popgen</a></p>")
-    
-  ), 
+  # title
+  titlePanel("Diploid model of selection with underdominance"),
   
-  mainPanel =  mainPanel(
-    plotOutput(outputId = 'viz'),
-    
-    selectInput("select", label = "Plot options", 
-                choices = list("Mean fitness by p" = 1,
-                               "Change in p by p" = 2,
-                               "Allele frequency over time" = 3,
-                               "Genotypes over time" = 4,
-                               "Mean fitness over time" = 5), selected = 4)
-    
-    
+  # top row is split into 3 columns of equal size (with 3 sliders)
+  fluidRow(
+    column(4,
+           sliderInput(inputId = "h",
+                       label = "Underdominance coefficient (h)",
+                       value = -0.2, 
+                       min = -1, max = 1,
+                       step = 0.1)
+           ),
+    column(4,
+           sliderInput(inputId = "p_init",
+                       label = "Initial allele frequency of A (p)",
+                       value = 0.32, 
+                       min = 0, max = 1,
+                       step = 0.02)
+           ),
+    column(4,
+           sliderInput(inputId = "gen",
+                       label = "Number of generations",
+                       value = 50, 
+                       min = 5, max = 1000,
+                       step = 5)
+          )
+    ),
+  # middle row is 2 columns of equal size
+  fluidRow(
+    # interactively display the model parameters
+    column(6,
+           "some text here?",
+           plotOutput("fitLand")),
+    # display the genotype frequencies over time
+    column(6,
+           plotOutput("genoTime")
+           )
+  ),
+  # bottom row is 2 columns of equal size
+  fluidRow(
+    # display delta p by p
+    column(6,
+           plotOutput("delta_p")),
+    # display the allele frequency over time
+    column(6,
+           plotOutput("alleleTime"))
   )
-))
-
-#back end code and response to user input
-server <- function(input, output){
   
-  output$viz <- renderPlot({
-    
-    p <- seq(0, 1, length.out = 1000)
-    #parameters
-    wAA = 1.2
-    h_under = input$h_under
-    waa = 1.0
-    p_0 = input$p_0
-    gen = (input$gen)+1
-    t = seq(0, gen, length.out = 1000)
-    
-    # calculate the fitness of the heterozygote using the dominance coefficient,
-    wAa <- 1 + h_under
-      
-    # make a plot of the fitness landscape
-    schematic_plot <- data.frame(Genotype = c("aa", "aA", "Aa", "AA"),
-                            Fitness = c(waa, rep(wAa, 2), wAA)) %>% 
-                    ggplot(aes(x=Genotype, y=Fitness, fill=Genotype)) +
-                      geom_bar(stat="identity") +
-                      scale_fill_manual(values = c("AA" = "firebrick", "Aa" = "orange", "aA" = "orange", "aa" = "gold")) +
-                      theme_classic()+theme(text = element_text(size=20),
-                                            legend.position="none")
-    
+)
 
-    
-    #if(input$w_plot){
-    if(input$select == 1){
-      W <- p^2*wAA + 2*p*(1-p)*wAa + (1-p)^2*waa
-      selected_plot <- data.frame(p=p, W=W) %>%
-        ggplot(aes(x = p, y = W)) +
-        geom_line(color="forestgreen", size=2) +
-        xlab("Mean Fitness") +
-        xlab("Allele frequency, p") +
-#        xlim(0, 1) +
-        scale_x_continuous(expand = c(0, 0), limits = c(0, 1)) + 
-        scale_y_continuous(expand = c(0, 0), limits = c(min(waa,wAa,wAA)-0.001, max(waa,wAa,wAA)+0.001)) + 
-#        theme(panel.background =  
-#                element_rect(fill =  rgb(30, 144, 255, 25, 
-#                                         maxColorValue = 255)),
-#              text = element_text(size=16, family= "Times"))
-        theme_classic()+theme(text = element_text(size=20))
-      
-      #} else if(input$delta_plot){
-    } else if(input$select == 2){
-      W <- p^2*wAA + 2*p*(1-p)*wAa + (1-p)^2*waa
-      delta_p <- (p^2*wAA + p*(1-p)*wAa) / W - p
-      setlim <- (max(waa,wAa,wAA)-min(waa,wAa,wAA))/(wAA + wAa + waa)
-      setlimround <- round(10*setlim)/10+0.1
-      selected_plot <- data.frame(p=p, delta_p=delta_p) %>%
-        ggplot(aes(x = p, y = delta_p)) +
-        geom_line(color="firebrick", size=2) +
-        geom_hline(yintercept = 0, lty = 2) +
-        ylab(expression(paste(Delta,p))) +
-        xlab("Allele frequency, p") +
-#        xlim(0, 1) +
-        scale_x_continuous(expand = c(0, 0), limits = c(0, 1)) + 
-        scale_y_continuous(expand = c(0, 0), limits = c(-setlimround,setlimround)) + 
-#        theme(panel.background =  
-#                element_rect(fill =  rgb(30, 144, 255, 25, 
-#                                         maxColorValue = 255)),
-#              text = element_text(size=16, family= "Times"))
-        theme_classic()+theme(text = element_text(size=20))
-      
-      #} else if(input$time_plot){
-    } else if(input$select == 3){
-      
-      p_t <- rep(NA, gen)
-      p_t[1] <- p_0
-      for(i in 2:gen){
-        W <- p_t[i-1]^2*wAA + 2*p_t[i-1]*(1-p_t[i-1])*wAa + (1-p_t[i-1])^2*waa
-        p_t[i] <- (p_t[i-1]^2 * wAA + p_t[i-1] * (1-p_t[i-1]) * wAa) / W
-      }
-      
-      selected_plot <- data.frame(t = 1:gen, p_t = p_t) %>%
-        ggplot(aes(x = t-1, y = p_t)) +
-        geom_point(color="firebrick", size=2) +
-        xlab("Generation") +
-        ylab("Allele frequency, p") +
-        annotate("text", x = 0.9*gen, y = 0.14, label = "Final p")+
-        annotate("text", x = 0.9*gen, y = 0.1, label = round(p_t[gen],5))+
-#        ylim(0, 1) + 
-        scale_x_continuous(expand = c(0, 0), limits = c(0, input$gen+2)) + 
-        scale_y_continuous(expand = c(0, 0), limits = c(0, 1.01)) + 
-#        theme(panel.background =  
-#                element_rect(fill =  rgb(30, 144, 255, 25, 
-#                                         maxColorValue = 255)),
-#              text = element_text(size=16, family= "Times"))
-        theme_classic()+theme(text = element_text(size=20))
-    } else if(input$select == 4){
-      
-      p_t <- rep(NA, gen)
-      p_t[1] <- p_0
-      for(i in 2:gen){
-        W <- p_t[i-1]^2*wAA + 2*p_t[i-1]*(1-p_t[i-1])*wAa + (1-p_t[i-1])^2*waa
-        p_t[i] <- (p_t[i-1]^2 * wAA + p_t[i-1] * (1-p_t[i-1]) * wAa) / W
-      }
-      
-      # simulate forward in time for each genotype
-      temp_df <- data.frame(t = 1:gen, p_t = p_t)
-      temp_df <- rbind(cbind(temp_df, y=(1-temp_df$p_t)^2, Genotype = "aa"),
-                       cbind(temp_df, y=2*temp_df$p_t*(1-temp_df$p_t), Genotype = "aA & Aa"),
-                       cbind(temp_df, y=temp_df$p_t^2, Genotype = "AA"))
-      temp_df$Genotype <- factor(temp_df$Genotype,
-                                 levels = c("aa", "aA & Aa", "AA"))
-        
-      selected_plot <-  temp_df %>%
-        ggplot(aes(x = t-1, y = y, colour=Genotype)) +
-        geom_point() +
-        scale_color_manual(values = c("AA" = "firebrick", "aA & Aa" = "orange", "aa" = "gold")) +
-        labs(x= "Generation",y="Genotype frequency",
-             color = "Legend") +
-        scale_x_continuous(expand = c(0, 0), limits = c(0, input$gen+2)) + 
-        scale_y_continuous(expand = c(0, 0), limits = c(0, 1.01)) + 
-        #        theme(panel.background =  
-        #                element_rect(fill =  rgb(30, 144, 255, 25, 
-        #                                         maxColorValue = 255)),
-        #              text = element_text(size=16, family= "Times")) 
-        labs(colour = "Genotypes") +
-        theme_classic()+theme(text = element_text(size=20),
-                              legend.position="bottom")
-    } else if(input$select == 5){
-      p_t <- rep(NA, gen)
-      W_t <- rep(NA, gen)
-      p_t[1] <- p_0
-      W_t[1] <- p_0^2*wAA + 2*p_0*(1-p_0)*wAa + (1-p_0)^2*waa
-      for(i in 2:gen){
-        W_t[i] <- p_t[i-1]^2*wAA + 2*p_t[i-1]*(1-p_t[i-1])*wAa + (1-p_t[i-1])^2*waa
-        p_t[i] <- (p_t[i-1]^2 * wAA + p_t[i-1] * (1-p_t[i-1]) * wAa) / W_t[i]
-      }
-      
-      selected_plot <- data.frame(t = 1:gen, W_t = W_t) %>%
-        ggplot(aes(x = t-1, y = W_t)) +
-        geom_point(color="forestgreen", size=2) +
-        xlab("Generation") +
-        ylab("Mean fitness") +
-        #        ylim(0, 1) + 
-        scale_x_continuous(expand = c(0, 0), limits = c(0, input$gen+2)) + 
-        scale_y_continuous(expand = c(0, 0), limits = c(min(waa,wAa,wAA)-0.001, max(waa,wAa,wAA)+0.001)) + 
-        #        theme(panel.background =  
-        #                element_rect(fill =  rgb(30, 144, 255, 25, 
-        #                                         maxColorValue = 255)),
-        #              text = element_text(size=16, family= "Times"))
-        theme_classic()+theme(text = element_text(size=20))
-    }
-    
-    return(wrap_plots(schematic_plot, 
-                      selected_plot, 
-                      nrow = 2, ncol = 1, byrow = FALSE))
-  })
+# Server logic
+server <- function(input, output) {
+  # for now fix the selection coefficient to 0.2
+  s <- 0.2
+  
+  ######################
+  # expressions
+  ######################
+  
+  # a reactive expression to get the fitness of the genotypes
+  w_genotypes <- reactive(fitness_genotypes(sel_coef = s,
+                                            dom_coef = input$h))
+  
+  # a reactive expression to simulate evolution of the system over time
+  sims <- reactive(sim_forward_time(num_gens = input$gen,
+                                    freq_init = input$p_init,
+                                    w_genotypes()))
+  
+  ######################
+  # outputs
+  ######################
+  
+  output$fitLand <- renderPlot({plot_schematic(w_genotypes())})
+  output$genoTime <- renderPlot({plot_t_genotypes(sims())})
+  output$delta_p <- renderPlot({plot_p_byp(w_genotypes())})
+  output$alleleTime <- renderPlot({plot_t_allele(sims())})
+  
 }
-# Run the application 
-shinyApp(ui = ui, server = server)
+
+# Complete app with UI and server components
+shinyApp(ui, server)
+
