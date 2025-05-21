8821d162d76470d8a566d22e6e52268dca0e8166
Ana-Hermina Ghenu <EvoNerd@users.noreply.github.com>
2025-05-21 16:30:09 +0200

    arrows, fast plotting, marginal w

--- app.R
+++ app.R
@@ -29,7 +29,11 @@ theme_set(fave_theme)
 update_geom_defaults("line", list(linewidth = 2.5, alpha=0.8))
 
 # define colour scheme for genotypes
-colours_genotypes <- c("AA" = "firebrick", "Aa" = "orange", "aA" = "orange", "aa" = "gold")
+  # use rgb to set transparency (alpha = 0.75) # note that max value for alpha is 255 so 0.75*255 ~ 191
+colours_genotypes <- c(AA = rgb(178, 34, 34, maxColorValue = 255, alpha = 191),  # Firebrick
+                       Aa = rgb(255, 165, 0, maxColorValue = 255, alpha = 191),  # Orange
+                       aA = rgb(255, 165, 0, maxColorValue = 255, alpha = 191),  # Orange
+                       aa = rgb(255, 215, 0, maxColorValue = 255, alpha = 191))  # Gold
 
 ###################################
 # define simulation functions
@@ -43,7 +47,7 @@ next_p <- function(w_AA, w_heterozygote, current_p, W_bar){
 
 # a function to get the fitness of the genotypes
 fitness_genotypes <- function(sel_coef, dom_coef)
-  return(c(aa = 1.0, aA = 1 + dom_coef, Aa = 1 + dom_coef, AA = 1 + sel_coef))
+  return(c(AA = 1 + sel_coef, Aa = 1 + dom_coef, aA = 1 + dom_coef, aa = 1.0))
 
 # a function to get mean fitness of the population
 mean_fitness <- function(w_genotypes, freq){
@@ -69,74 +73,197 @@ sim_forward_time <- function(num_gens, freq_init, w_genotypes){
   return(df)
 }
 
+# a function to get the delta p values
+get_p_byp <- function(w_genotypes) {
+  # Initialize a vector of allele frequencies from 0 to 1
+  p_values <- seq(0, 1, length.out = 1000)
+  
+  # Compute mean fitness and change in allele frequency
+  W_bar_values <- sapply(p_values, function(p) mean_fitness(w_genotypes, p))
+  delta_p_values <- sapply(1:length(p_values), function(i) 
+    next_p(w_genotypes["AA"], w_genotypes["Aa"], p_values[i], W_bar_values[i]) - p_values[i])
+  
+  return(data.frame(p=p_values, delta_p=delta_p_values))
+}
+
+# a function indicating the long-term steady state
+get_steady_state <- function(w_genotypes, deltap.df){
+  # some presets for plotting the arrows
+  min_arrow_length = 0.07
+  arrow_vertical_whitespace = 0.1
+  
+  # initialize output variable with NA values:
+    # depending on outcome, only some may be replaced with non-NA values
+  output <- list(outcome = NA, stable_pts = NA, unstable_pt = NA, arrow1 = NA, arrow2 = NA)
+  
+  # underdominance always results in bistability
+  if(w_genotypes["Aa"] < w_genotypes["aa"]) {
+    # a hacky way to approximate where the unstable equilibrium point is:
+    unstable_pt <- mean(deltap.df$p[which(near(deltap.df$delta_p[12:990], 0, tol = 10^-3))])
+    # draw the arrows symmetrically about x=0 at halfway of the smallest max delta_p values
+    x_val = min(abs(min(deltap.df$delta_p)), abs(max(deltap.df$delta_p)))/2
+    # y values for bottom arrow (1): vertically in middle of the 2 equil pts
+    arrow1_length <- max(unstable_pt-2*arrow_vertical_whitespace, min_arrow_length)
+    # y values for top arrow (2): vertically in middle of the 2 equil pts
+    arrow2_length <- max(1-unstable_pt-2*arrow_vertical_whitespace, min_arrow_length)
+    output <- list(outcome = "bistable",
+                stable_pts = c(0, 1),
+                unstable_pt = unstable_pt,
+                arrow1 = c(x0 = -x_val, y0 = unstable_pt - arrow_vertical_whitespace,
+                           x1 = -x_val, y1 = unstable_pt - arrow_vertical_whitespace - arrow1_length),
+                arrow2 = c(x0 = x_val, y0 = unstable_pt + arrow_vertical_whitespace,
+                           x1 = x_val, y1 = unstable_pt + arrow_vertical_whitespace + arrow2_length)
+                )
+    
+  # additivity / partial dominance always results in A going to fixation (1 stable attractor)
+  } else if(w_genotypes["Aa"] >= w_genotypes["aa"] & 
+            w_genotypes["Aa"] < w_genotypes["AA"]) {
+    x_val <- 0.005 # this x-value looks nice
+    # increase the whitespace of the arrow
+    arrow_vertical_whitespace <- 3*arrow_vertical_whitespace
+    arrow1_length <- 1-2*arrow_vertical_whitespace
+    output <- list(outcome = "p=1",
+                   stable_pts = 1,
+                   arrow1 = c(x0 = x_val, y0 = arrow_vertical_whitespace,
+                              x1 = x_val, y1 = arrow_vertical_whitespace + arrow1_length)
+                   )
+    
+  # overdominance always results in 1 stable attractor at intermediate allele freq  
+  } else if(w_genotypes["Aa"] > w_genotypes["AA"]){
+    # a hacky way to approximate where the stable equilibrium point is:
+    stable_pt <- mean(deltap.df$p[which(near(deltap.df$delta_p[5:980], 0, tol = 10^-3))])
+    # draw the arrows symmetrically about x=0 at one third of the smallest max delta_p values
+    x_val = min(abs(min(deltap.df$delta_p)), abs(max(deltap.df$delta_p)))/3
+    # y values for bottom arrow (1): vertically in middle of the 2 equil pts
+    arrow1_length <- max(stable_pt-2*arrow_vertical_whitespace, min_arrow_length)
+    # y values for top arrow (2): vertically in middle of the 2 equil pts
+    arrow2_length <- max(1-stable_pt-2*arrow_vertical_whitespace, min_arrow_length)
+    output <- list(outcome = "p intermediate",
+                   stable_pts = stable_pt,
+                   arrow1 = c(x0 = x_val, y0 = stable_pt - arrow_vertical_whitespace - arrow1_length,
+                              x1 = x_val, y1 = stable_pt - arrow_vertical_whitespace),
+                   arrow2 = c(x0 = -x_val, y0 = stable_pt + arrow_vertical_whitespace + arrow2_length,
+                              x1 = -x_val, y1 = stable_pt + arrow_vertical_whitespace)
+                   )
+  } 
+
+  
+  return(output)
+}
+
 ###################################
 # define visualization functions
 ###################################
 
 # plot fitness of the genotypes
 plot_schematic <- function(w_genotypes) {
-  # create df for nice plotting
-  df <- data.frame(Fitness = w_genotypes,
-                   Genotype = names(w_genotypes))
-  # make genotype a factor with levels for nice plotting
-  df$Genotype <- factor(df$Genotype,
-                        levels = c("AA", "Aa", "aA", "aa"))
-  # boxplot of fitness
-  ggplot(df,
-         aes(x = Genotype, y = Fitness, fill = Genotype)) +
-    geom_bar(stat="identity") +
-    scale_fill_manual(values = colours_genotypes) +
-    theme(legend.position="none") +
-    scale_y_continuous(limits = c(0, 2), expand = c(0, 0))
+  # change the graphing settings
+  par(cex.lab = 2, # increase size of axis labels
+      cex.axis = 1.5, # increase size of tick mark labels 
+      mar = c(4.2, 4.4, 1, 0.1)) # decrease the borders
+  
+  # make the barplot in graphics (i.e., base R)
+  barplot(
+    w_genotypes, 
+    col = colours_genotypes[names(w_genotypes)], 
+    border = "black", 
+    ylim = c(0, 2), 
+    ylab = "Fitness",
+    xlab = "Genotype"
+  )
+  
+  # add a box around the whole plot
+  box()
 }
 
 # a function to plot the genotypes over time
-plot_t_genotypes <- function(sim_df){
-  # remove the column for allele frequency and change the df to long format
-  plot_df <- sim_df %>% select(-p) %>%
-    pivot_longer(!gens, names_to="genotype", values_to="freq")
-  # make genotype a factor with levels for nice plotting
-  plot_df$genotype <- factor(plot_df$genotype,
-                             levels = c("AA", "hetero", "aa"))
-  # rename the factor levels for nice plotting
-  levels(plot_df$genotype) <- c("AA", "aA & Aa", "aa")
-  ggplot(plot_df,
-         aes(x=gens, y=freq, colour=genotype)) +
-    geom_line() +
-    scale_color_manual(values = unname(colours_genotypes)[-2]) +
-    labs(x= "Generation", y="Genotype frequency", color = "Legend") +
-    scale_y_continuous(limits = c(-0.01, 1.01), expand = c(0, 0)) +
-    # can try removing the legend altogether
-    theme(legend.position="none")
+plot_t_genotypes <- function(sim_df) {
+  # Define colors for genotypes
+  temp_colours <- colours_genotypes[c(1,2,4)]
+  
+  # change the graphing settings
+  par(cex.lab = 2, # increase size of axis labels
+      cex.axis = 1.5, # increase size of tick mark labels 
+      mar = c(4.2, 4.4, 0.7, 0.85)) # decrease the borders
+  
+  # Set up an empty plot
+  plot(sim_df$gens, sim_df$AA, type = "n",
+       ylim = c(-0.01, 1.01), yaxs = "i", # remove y-axis padding
+       xlab = "Generation", ylab = "Genotype frequency")
+  
+  # Add lines for each genotype
+  lines(sim_df$gens, sim_df$AA, col = temp_colours[1], lwd = 7)
+  lines(sim_df$gens, sim_df$hetero, col = temp_colours[2], lwd = 7)
+  lines(sim_df$gens, sim_df$aa, col = temp_colours[3], lwd = 7)
+  
+  # Optionally, add a legend
+  #legend("topright", legend = c("AA", "aA & Aa", "aa"), col = temp_colours, 
+   #      lty = 1, lwd = 2.5, bty = "n")
 }
+
 
 # a function to plot the allele frequency over time
-plot_t_allele <- function(sim_df){
-  # keep just the columns for time and allele frequency
-  plot_df <- sim_df %>% select(gens, p)
-  ggplot(plot_df,
-         aes(x=gens, y=p)) +
-    geom_line() +
-    labs(x= "Generation", y="Allele frequency") +
-    scale_y_continuous(limits = c(-0.01, 1.01), expand = c(0, 0))
+plot_t_allele <- function(sim_df) {
+  # change the graphing settings
+  par(cex.lab = 2, # increase size of axis labels
+      cex.axis = 1.5, # increase size of tick mark labels 
+      mar = c(4.2, 4.4, 0.7, 0.85)) # decrease the borders
+  
+  # Set up an empty plot
+  plot(sim_df$gens, sim_df$p, type = "n",
+       ylim = c(-0.01, 1.01), yaxs = "i", # remove y-axis padding
+       xlab = "Generation", ylab = "Allele frequency")
+  
+  # Add the line
+  lines(sim_df$gens, sim_df$p, col = "black", lwd = 7)
 }
 
-# a function to plot delta p
-plot_p_byp <- function(w_genotypes){
-  # initialize a vector of allele frequencies from 0 to 1
-  plot_df <- data.frame(p = seq(0, 1, length.out = 1000)) %>%
-    # get the mean fitness at each allele frequency
-    mutate(W_bar = mean_fitness(w_genotypes, p),
-           # get delta_p by current allele frequency from that in the next generation
-           delta_p = next_p(w_genotypes["AA"], w_genotypes["Aa"], p, W_bar) - p)
-  # figure out how to draw the arrows
-  # TO DO!!
-  ggplot(plot_df,
-         aes(x=delta_p, y=p)) +
-    geom_vline(xintercept = 0, linewidth=0.6, linetype = "dotted") +
-    geom_point() +
-    labs(x= expression("Change in allele frequency (" * Delta * "p)"), y="Allele frequency") +
-    scale_y_continuous(limits = c(-0.01, 1.01), expand = c(0, 0))
+plot_p_byp <- function(deltap.df, steady_state) {
+  # change the graphing settings
+  par(cex.lab = 2, # increase size of axis labels
+      cex.axis = 1.5, # increase size of tick mark labels 
+      mar = c(4.2, 4.4, 0.7, 0.85)) # decrease the borders
+  
+  # plot the points
+  plot(x = deltap.df$delta_p, y = deltap.df$p,
+       pch = 16, col = "black", 
+       xlab = expression("Change in allele frequency (" * Delta * "p)"), 
+       ylab = "Allele frequency", xlim = range(deltap.df$delta_p),
+       ylim = c(-0.01, 1.01), yaxs = "i")
+  
+  # plot the arrows
+  if(steady_state$outcome == "bistable"){
+    arrows(x0 = steady_state$arrow1["x0"],
+           y0 = steady_state$arrow1["y0"],
+           x1 = steady_state$arrow1["x1"],
+           y1 = steady_state$arrow1["y1"],
+           length=0.2)
+    arrows(x0 = steady_state$arrow2["x0"],
+           y0 = steady_state$arrow2["y0"],
+           x1 = steady_state$arrow2["x1"],
+           y1 = steady_state$arrow2["y1"],
+           length=0.2)
+  } else if(steady_state$outcome == "p=1"){
+    arrows(x0 = steady_state$arrow1["x0"],
+           y0 = steady_state$arrow1["y0"],
+           x1 = steady_state$arrow1["x1"],
+           y1 = steady_state$arrow1["y1"],
+           length=0.2)
+  } else if(steady_state$outcome == "p intermediate"){
+    arrows(x0 = steady_state$arrow1["x0"],
+           y0 = steady_state$arrow1["y0"],
+           x1 = steady_state$arrow1["x1"],
+           y1 = steady_state$arrow1["y1"],
+           length=0.2)
+    arrows(x0 = steady_state$arrow2["x0"],
+           y0 = steady_state$arrow2["y0"],
+           x1 = steady_state$arrow2["x1"],
+           y1 = steady_state$arrow2["y1"],
+           length=0.2)
+  }
+  
+  # Add a dotted vertical line at x = 0
+  abline(v = 0, lty = 3, lwd = 0.6, col = "black")
 }
 
 
@@ -171,7 +298,7 @@ ui <- fluidPage(
            sliderInput(inputId = "gen",
                        label = "Number of generations",
                        value = 50, 
-                       min = 5, max = 1000,
+                       min = 5, max = 950,
                        step = 5)
           )
     ),
@@ -190,48 +317,57 @@ ui <- fluidPage(
   fluidRow(
     # display delta p by p
     column(6,
-           plotOutput("delta_p")),
+           plotOutput("delta_p")
+           ),
     # display the allele frequency over time
     column(6,
-           plotOutput("alleleTime"))
+           plotOutput("alleleTime")
+           )
   )
   
 )
 
 # Server logic
 server <- function(input, output) {
-  # for now fix the selection coefficient to 0.2
-  s <- 0.2
+  # fix the selection coefficient
+    # be careful to choose a value that does *NOT* result in neutral system
+  s <- 0.206
   
   ######################
   # expressions
   ######################
   
   # a reactive expression to get the fitness of the genotypes
-  w_genotypes <- reactive(fitness_genotypes(sel_coef = s,
-                                            dom_coef = input$h))
+  w_gent <- reactive(fitness_genotypes(sel_coef = s,
+                                       dom_coef = input$h))
+  
+  # a reactive expression to get delta p as a function of allele frequecy (p)
+  delta_p <- reactive(get_p_byp(w_gent()))
+  
+  # a reactive expression to get the long-term model outcome
+  outcome <- reactive(get_steady_state(w_genotypes = w_gent(), deltap.df = delta_p()))
   
   # a reactive expression to simulate evolution of the system over time
   sims <- reactive(sim_forward_time(num_gens = input$gen,
                                     freq_init = input$p_init,
-                                    w_genotypes()))
+                                    w_gent()))
   
   ######################
   # outputs
   ######################
   
   output$dynamEq <- renderUI({
-    temp_genotypes <- w_genotypes()
+    temp_genotypes <- w_gent()
     w_AA <- temp_genotypes["AA"]
     w_Aa <- temp_genotypes["Aa"]
     w_aa <- temp_genotypes["aa"]
     
-    withMathJax(sprintf("$$p_{t+1} = \\frac{p_t^2 \\cdot %.01f + p_t(1-p_t) \\cdot %.01f}{p_t^2 \\cdot %.01f + 2p_t(1-p_t) \\cdot %.01f + (1-p_t)^2 \\cdot %.01f}$$",
+    withMathJax(sprintf("$${\\tiny p_{t+1} = \\frac{p_t^2 \\cdot (1+s) + p_t(1-p_t) \\cdot (1+s+h)}{\\overline{w}} = \\frac{p_t^2 \\cdot %.01f + p_t(1-p_t) \\cdot %.01f}{p_t^2 \\cdot %.01f + 2p_t(1-p_t) \\cdot %.01f + (1-p_t)^2 \\cdot %.01f}}$$",
                         w_AA, w_Aa, w_AA, w_Aa, w_aa))
   })
-  output$fitLand <- renderPlot({plot_schematic(w_genotypes())})
+  output$fitLand <- renderPlot({plot_schematic(w_gent())})
   output$genoTime <- renderPlot({plot_t_genotypes(sims())})
-  output$delta_p <- renderPlot({plot_p_byp(w_genotypes())})
+  output$delta_p <- renderPlot({plot_p_byp(delta_p(), outcome())})
   output$alleleTime <- renderPlot({plot_t_allele(sims())})
   
 }
